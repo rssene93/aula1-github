a78473167b58eff92d8ca5d4283ee84287ba2549
Raphael Sene <raphael.sene@gmail.com>
2021-02-26 18:33:23 -0300

    Mensagem de boa noite

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("BOM DIA!");
 		System.out.println("BOA TARDE!");
-
+		System.out.println("BOA NOITE");
 	}
 
 }
